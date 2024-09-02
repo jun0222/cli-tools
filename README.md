17e72192f81ddea4615c2ff3baa8be65e19164ea
JunHatanaka <nextseason6@gmail.com>
2024-09-03 01:50:44 +0900

    chore: create repo && autocommit

---
+++ README.md
@@ -0,0 +1,7 @@
+## TODO
+
+- [ ] pre-commit
+- [ ] ci 系
+- [ ] 便利ツール
+- [ ] md2html
+- [ ] html をビルドするツール
---
+++ autocommit_md_1file/bash.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# 更新されたファイルのパスを取得
+updated_file="ファイル名.md"
+
+# ファイルの1行目を取得
+first_line=$(head -n 1 "$updated_file" | sed "s/#//g")
+
+# Gitに追加
+git add "$updated_file"
+
+# 1行目をコミットメッセージとして使用してコミット
+git commit -m "autocommit: $first_line"
+
+git push origin HEAD
