1f7c4ca6b65c539b050d6e046ab28c4dcc297f77
JunHatanaka <nextseason6@gmail.com>
2024-09-03 19:17:09 +0900

    autocommit

---
+++ autocommit_only_md_1and_all_push/bash.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# 指定ファイルに関しては、内容を見てコミットメッセージに反映する
+updated_file="output.md"
+first_line=$(head -n 1 "$updated_file" | sed "s/#//g")
+git add "$updated_file"
+git commit -m "autocommit: $first_line"
+
+# それ以外のファイルは、全ての変更を同じメッセージでコミットする
+git add .
+git commit -m "autocommit"
